c23047010247be8ba8e7c6ec20fe5e2758dc3263
Thant Zin Htun <thantz6745@gmail.com>
2026-06-15 21:47:50 +0630

    raw react complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import {useState, useRef} from 'react';
+function Item({name}){
+  return <ul>
+    <li>{name}</li>
+  </ul>
+}
+
+
 export default function App(){
+  const inputRef = useRef();
+   let [data , setData] = useState([
+    {id:4, name: "Apple", done: true},
+    {id:3, name: "Orange", done: false},
+    {id:2, name: "Bread", done: true},
+    {id:1, name: "Egg", done: true},
+  ]);
+  
+
+const add = () => {
+  let id = data[0].id + 1;
+  let name = inputRef.current.value;
+  setData([{id, name, done: false}, ...data]);
+}
+
   return <div>
-    Hello React
+      <h1>Hello React</h1>
+      <form onSubmit={e => {
+        e.preventDefault();
+        add();
+        e.currentTarget.reset();
+      }
+      }>
+        <input type='text' ref={inputRef} />
+        <button type='submit'>ADD</button>
+      </form>
+      
+      {data.map(item => <Item key={item.id} name={item.name}/> )}
     </div>
 
 }
